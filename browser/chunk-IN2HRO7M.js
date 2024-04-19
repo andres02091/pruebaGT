d0b8838e6924b23e357674e8c24ebc4211eb50f5
andres02091 <urqiza02091@gmail.com>
2024-04-19 17:00:02 -0500

    cambios

---
+++ browser/chunk-IN2HRO7M.js
@@ -0,0 +1,1 @@
+import{Va as o,Wa as a,aa as i,cb as r,ib as s}from"./chunk-O26KBKTK.js";var m=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=i({type:e,selectors:[["app-skill"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(o(0,"p"),r(1,"skill works!"),a())}});let t=e;return t})();export{m as SkillComponent};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4VZN2JMC.mjs";import"./chunk-VZQIMERO.mjs";import"./chunk-SMARMCMH.mjs";import"./chunk-S4NWIVL2.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
