ab7e8b198ac2b8a13d9602c91ad93199d34c0dc7
andres02091 <urqiza02091@gmail.com>
2024-04-22 08:06:09 -0500

    Cambios

--- server/chunk-BQVJHNXA.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Va as o,Wa as r,Z as i,cb as a,ib as p}from"./chunk-S4NWIVL2.mjs";import"./chunk-KRLCULJA.mjs";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=i({type:e,selectors:[["app-experience"]],standalone:!0,features:[p],decls:2,vars:0,template:function(n,c){n&1&&(o(0,"p"),a(1,"experience works!"),r())}});let t=e;return t})();export{d as ExperienceComponent};
--- server/chunk-JQCI5AMA.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Va as o,Wa as a,Z as i,cb as r,ib as s}from"./chunk-S4NWIVL2.mjs";import"./chunk-KRLCULJA.mjs";var m=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=i({type:e,selectors:[["app-skill"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(o(0,"p"),r(1,"skill works!"),a())}});let t=e;return t})();export{m as SkillComponent};
